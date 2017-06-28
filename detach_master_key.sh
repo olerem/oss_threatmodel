81f9eed10407bf144d28e8f09f03ccf97455a5a0
Oleksij Rempel <linux@rempel-privat.de>
2017-06-28 12:23:40 +0200

    Update detach_master_key.sh

--- detach_master_key.sh
+++ detach_master_key.sh
@@ -7,6 +7,10 @@ MASTERKEY="$1"
 # Detach Master Key From Subkey
 gpg --export-secret-key $MASTERKEY > private.key
 gpg --export $MASTERKEY >> private.key
+# don't forget to backup the private.key
+gpg --export $MASTERKEY > public.key
+#TODO, how to find proper subkay for export?
+gpg --export-secret-subkeys A72DB3EF > signing.key
 gpg --delete-secret-key $MASTERKEY
 gpg --import public.key signing.key
 gpg --send-key $MASTERKEY
